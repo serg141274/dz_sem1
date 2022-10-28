d4a6955ced647882a4efa4e82e7c0323363b01e5
Sergej <sheshnsergej@mail.ru>
2022-10-28 16:59:42 +0300

    сем1

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+ 
+/*Console.WriteLine("Input first number");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA > numB)
+{
+Console.WriteLine($"{numA} больше чем {numB}");    
+}
+else
+{
+Console.WriteLine($"{numB} больше чем {numA}");
+}    
+*/
+///////////////////////////////////////
+/*Задача 4: Напишите программу, которая принимает на вход три числа
+ и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+/*Console.WriteLine("Input first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input three number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max) 
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}    
+Console.WriteLine("максимальное"  + max);*/
+////////////////////
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+ является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет */
+
+/*Console.WriteLine("Input namber");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 1)
+{
+ Console.WriteLine($"число  {num}   нечетное ");
+}
+else
+{ 
+    Console.WriteLine($"число   {num}   четное ");
+} */
+//////////////////////
+/*Задача 8: Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+/*Console.WriteLine("Input n");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1 ;i <= n ;i ++)    
+if(i % 2 == 0 )
+{
+Console.Write($" {i} ");
+} */
